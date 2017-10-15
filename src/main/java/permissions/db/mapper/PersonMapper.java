ea2f9d0459030b453e81b9f119020fa3ed3c8297
Krzysztof Pawlowski <krzysztof.pawlowski@gmail.com>
2017-10-15 11:49:26 +0200

    introduced consts for sql stmts

--- src/main/java/permissions/db/mapper/PersonMapper.java
+++ src/main/java/permissions/db/mapper/PersonMapper.java
@@ -13,9 +13,58 @@ import java.util.List;
 public class PersonMapper extends AbstractMapper<Person> {
 
     private static final String COLUMNS = "id, name, surname";
+    public static final String SELECT_BY_NAME_STMT = "SELECT " + COLUMNS + " FROM person WHERE name=? OFFSET ? LIMIT ?";
+    public static final String SELECT_BY_SURNAME_STMT =
+        "SELECT " + COLUMNS + " FROM person WHERE Surname=? OFFSET ? LIMIT ?";
+    public static final String FIND_ALL_ON_PAGE_STMT =
+        "SELECT " + COLUMNS + " FROM people" + " WHERE id=? OFFSET ? LIMIT ?";
+    public static final String FIND_STMT = "SELECT " + COLUMNS + " FROM people" + " WHERE id=?";
+    public static final String INSERT_STMT = "INSERT INTO person(name,surname) VALUES(?,?)";
+    public static final String UPDATE_STMT = "UPDATE person SET (name,surname)=(?,?) WHERE id=?";
+    public static final String DELETE_STMT = "DELETE FROM person WHERE id=?";
 
     public PersonMapper(Connection connection) {
         super(connection);
+    }
+
+    public List<Person> withSurname(String surname, PagingInfo page) {
+
+        List<Person> result = new ArrayList<Person>();
+        PreparedStatement selectBySurname;
+        try {
+            selectBySurname = connection.prepareStatement(SELECT_BY_SURNAME_STMT);
+            selectBySurname.setString(1, surname);
+            selectBySurname.setInt(2, page.getCurrentPage() * page.getSize());
+            selectBySurname.setInt(3, page.getSize());
+            ResultSet rs = selectBySurname.executeQuery();
+            while (rs.next()) {
+                Person person = doLoad(rs);
+                result.add(person);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public List<Person> withName(String name, PagingInfo page) {
+
+        List<Person> result = new ArrayList<Person>();
+        PreparedStatement selectByName;
+        try {
+            selectByName = connection.prepareStatement(SELECT_BY_NAME_STMT);
+            selectByName.setString(1, name);
+            selectByName.setInt(2, page.getCurrentPage() * page.getSize());
+            selectByName.setInt(3, page.getSize());
+            ResultSet rs = selectByName.executeQuery();
+            while (rs.next()) {
+                Person person = doLoad(rs);
+                result.add(person);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
@@ -42,75 +91,26 @@ public class PersonMapper extends AbstractMapper<Person> {
 
     @Override
     protected String findStatement() {
-        return "SELECT " + COLUMNS + " FROM people" + " WHERE id=?";
+        return FIND_STMT;
     }
 
     @Override
     protected String findAllOnPageStatement() {
-        return "SELECT " + COLUMNS + " FROM people" + " WHERE id=? OFFSET ? LIMIT ?";
+        return FIND_ALL_ON_PAGE_STMT;
     }
 
     @Override
     protected String insertStatement() {
-        return "INSERT INTO person(name,surname) VALUES(?,?)";
+        return INSERT_STMT;
     }
 
     @Override
     protected String updateStatement() {
-        return "UPDATE person SET (name,surname)=(?,?) WHERE id=?";
+        return UPDATE_STMT;
     }
 
     @Override
     protected String removeStatement() {
-        return "DELETE FROM person WHERE id=?";
+        return DELETE_STMT;
     }
-
-    public List<Person> withSurname(String surname, PagingInfo page) {
-
-        List<Person> result = new ArrayList<Person>();
-        PreparedStatement selectBySurname;
-        try {
-            selectBySurname = connection.prepareStatement(selectBySurnameStatement());
-            selectBySurname.setString(1, surname);
-            selectBySurname.setInt(2, page.getCurrentPage() * page.getSize());
-            selectBySurname.setInt(3, page.getSize());
-            ResultSet rs = selectBySurname.executeQuery();
-            while (rs.next()) {
-                Person person = doLoad(rs);
-                result.add(person);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-    private String selectBySurnameStatement() {
-        return "SELECT " + COLUMNS + " FROM person WHERE Surname=? OFFSET ? LIMIT ?";
-    }
-
-    private String selectByNameStatement() {
-        return "SELECT " + COLUMNS + " FROM person WHERE name=? OFFSET ? LIMIT ?";
-    }
-
-    public List<Person> withName(String name, PagingInfo page) {
-
-        List<Person> result = new ArrayList<Person>();
-        PreparedStatement selectByName;
-        try {
-            selectByName = connection.prepareStatement(selectByNameStatement());
-            selectByName.setString(1, name);
-            selectByName.setInt(2, page.getCurrentPage() * page.getSize());
-            selectByName.setInt(3, page.getSize());
-            ResultSet rs = selectByName.executeQuery();
-            while (rs.next()) {
-                Person person = doLoad(rs);
-                result.add(person);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
 }
